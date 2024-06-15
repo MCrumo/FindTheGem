9a4e71cc5cb76228218891c6b18192f0b1540f06
MCrumo <miquel1rr@gmail.com>
2024-06-15 17:15:58 +0200

    camps de patentes aegits

--- backend/FTGapp/models.py
+++ backend/FTGapp/models.py
@@ -4,5 +4,15 @@ from django.db import models
 
 #El frontend els ha de nombrar igual, els atributs de Patents
 class Patent(models.Model):
-    titol = models.TextField(max_length=255, blank = False) #no es pot deixar en blanc
-    cos = models.TextField(blank=True, null=True) #null es que que pot ser null
+    aplitactionNumber = models.TextField(max_length=32, blank = False)
+    publicationNumber = models.TextField(max_length=32, blank = False)
+    title = models.TextField(max_length=32, blank = False) #no es pot deixar en blanc
+    status = models.TextField(max_length=32, blank = False) 
+    country = models.TextField(max_length=32, blank = False) 
+    grantDate = models.TextField(max_length=32, blank = False) 
+    expirationDate = models.TextField(max_length=32, blank = False) 
+    sizeFamily = models.TextField(max_length=32, blank = False) 
+    numberCitations = models.TextField(max_length=32, blank = False) 
+    score = models.TextField(max_length=64, blank = False) 
+   
+    # EXAMPELE cos = models.TextField(blank=True, null=True) #null es que que pot ser null
